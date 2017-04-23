7904802b8543a00b4d44c87a844df689ef3f68a3
Ursula <ursula@heinrich-foto.de>
2017-04-23 14:10:20 +0200

    code is adjusted in order to work with ORBSLAM2

---
+++ src/limitless_octree_creator.cpp
@@ -0,0 +1,138 @@
+/*
+*
+*OCTOSLAM
+*Octoslam is a ROS package which converts files in Polygon File Format(ply) to Binary Terrain Files(bt).
+*This software has been developed by using Octomap- An Efficient Probabilistic 3D Mapping Framework Based on Octrees as infrastructure.
+*http://octomap.github.com/.
+*
+*To find detailed information please visit following address
+*https://github.com/yng05/octoslam/wiki
+*
+*Bauhaus University Weimar 2016-17
+*
+*/
+
+#include <string> 
+#include <sstream> 
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/foreach.hpp>
+
+#include <octomap_msgs/conversions.h>
+#include <octomap_msgs/Octomap.h>
+#include <octomap_msgs/GetOctomap.h>
+#include <octomap_ros/conversions.h>
+
+#include <ctime>
+
+using namespace std;
+using namespace octomap;
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+OcTree tree (0.1);
+double durationTotalConversion = 0.0;
+double durationTotal = 0.0;
+std::clock_t startListening;
+bool isFirstListened = true;
+int mapSize = 0;
+
+void print_query_info(point3d query, OcTreeNode* node) {
+  if (node != NULL) {
+    cout << "occupancy probability at " << query << ":\t " << node->getOccupancy() << endl;
+  }
+  else 
+    cout << "occupancy probability at " << query << ":\t is unknown" << endl;    
+}
+
+void grow_map(const PointCloud::ConstPtr& pCloud){
+
+  BOOST_FOREACH (const pcl::PointXYZ& pt, pCloud->points){
+    point3d endpoint ((float) pt.x, (float) pt.y, (float) pt.z);
+    tree.updateNode(endpoint, true);
+  }
+
+  
+  point3d query (0., 0., 0.);
+  OcTreeNode* result = tree.search (query);
+  print_query_info(query, result);
+
+  query = point3d(-1.,-1.,-1.);
+  result = tree.search (query);
+  print_query_info(query, result);
+
+  query = point3d(1.,1.,1.);
+  result = tree.search (query);
+  print_query_info(query, result);
+}
+
+void sendOctomap()
+{
+  /*cout << msg->data.c_str() << endl;
+
+  durationTotal = ( std::clock() - startListening) / (double) CLOCKS_PER_SEC;
+  isFirstListened = true;
+
+
+  cout << "Start: " << startListening / (double) CLOCKS_PER_SEC << " end" << std::clock() / (double) CLOCKS_PER_SEC << endl;
+
+  cout << "Total Duration: " << durationTotal << " second" << endl;
+  cout << "Total Duration for conversions: " << durationTotalConversion << " second" << endl;*/
+  
+  octomap_msgs::Octomap bmap_msg;
+  octomap_msgs::binaryMapToMsg(tree, bmap_msg);
+  ros::NodeHandle n;
+
+  ros::Publisher octomap_publisher = n.advertise<octomap_msgs::Octomap>("octree",1);
+
+
+  ros::Rate loop_rate(10);
+  while (ros::ok())
+  {
+    octomap_publisher.publish(bmap_msg);
+    loop_rate.sleep();
+  }
+
+}
+
+void pointCloudCallback(const PointCloud::ConstPtr& msg)
+{
+  mapSize++;
+  if(isFirstListened){
+    startListening = std::clock();
+    isFirstListened = false;
+  }
+  printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
+  cout << msg->header.frame_id << endl;
+
+  std::clock_t start;
+  double duration;
+
+  start = std::clock();
+  grow_map(msg);  
+  duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+  durationTotalConversion += duration;
+
+  std::cout<<"duration: "<< duration <<" second"<<'\n';
+  
+  if(mapSize>20){
+    mapSize = 0;
+    sendOctomap();
+  }
+}
+
+int main(int argc, char** argv) {
+  cout << "listening point cloud publisher..." << endl;
+  ros::init(argc, argv, "octree_creator");
+  ros::NodeHandle n; 
+  ros::Subscriber sub = n.subscribe("pointcloud", 1000, pointCloudCallback);
+  ros::spin();
+
+  return 0;
+
+}
+
